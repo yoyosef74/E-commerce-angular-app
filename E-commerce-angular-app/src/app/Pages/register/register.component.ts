0996cb007b9869ad89a60ddd989945e2be512207
Youssef Ashraf <yoyosef74@yahoo.com>
2025-05-16 21:19:57 +0300

    Adding Auth footer - Handling Register form error msgs

--- E-commerce-angular-app/src/app/Pages/register/register.component.ts
+++ E-commerce-angular-app/src/app/Pages/register/register.component.ts
@@ -4,11 +4,21 @@ import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { Message } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    ButtonModule,
+    MessagesModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -34,7 +44,9 @@ export class RegisterComponent {
     return (formGroup: AbstractControl): { [key: string]: any } | null => {
       const password = formGroup.get('password')?.value;
       const rePassword = formGroup.get('rePassword')?.value;
+      const rePasswordControl = formGroup.get('rePassword') as FormControl;
       if (password !== rePassword) {
+        rePasswordControl.setErrors({ passNotMatch: true });
         return { passNotMatch: true };
       }
       return null;
@@ -43,6 +55,19 @@ export class RegisterComponent {
 
   submit(){
     console.log(this.registrationForm)
+  }
+
+  get name() {
+    return this.registrationForm.get('name');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get password() {
+    return this.registrationForm.get('password');
+  }
+  get rePassword() {
+    return this.registrationForm.get('rePassword');
   }
 
   // name!: FormControl
